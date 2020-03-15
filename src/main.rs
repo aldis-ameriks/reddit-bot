118d9d496d6e1e4a45fbbeb0dbb9c32b84c745c8
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-03-15 19:20:57 +0200

    log warning for missing .env file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
 
-use log::Level;
+use log::{warn, Level};
 
 use dotenv::dotenv;
 use reddit_bot::start;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    simple_logger::init_with_level(Level::Info).expect("Failed to init logger");
-    let token = env::var("TG_TOKEN").expect("Missing TG_TOKEN env var");
+    simple_logger::init_with_level(Level::Info).expect("failed to init logger");
+
+    if let Err(err) = dotenv() {
+        warn!("failed to load .env file: {}", err);
+    }
+
+    let token = env::var("TG_TOKEN").expect("missing TG_TOKEN env var");
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     start(token, database_url).await?;

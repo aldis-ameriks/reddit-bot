89062e39e5a3dd6000594f7ddf891d7a8a44fcc2
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-03-15 18:58:58 +0200

    simplify partialeq

--- src/reddit/post.rs
+++ src/reddit/post.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Post {
     pub title: String,
     pub link: String,
@@ -9,15 +9,5 @@ pub struct Post {
 impl fmt::Display for Post {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}\n{}\n", self.title, self.link)
-    }
-}
-
-impl std::cmp::PartialEq for Post {
-    fn eq(&self, other: &Self) -> bool {
-        self.title == other.title && self.link == other.link
-    }
-
-    fn ne(&self, other: &Self) -> bool {
-        self.title != other.title || self.link != other.link
     }
 }
